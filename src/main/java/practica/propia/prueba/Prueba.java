41ecda99d8f12514c6c06136cc8d644cfb4b990b
Sandy <sandymartinezmejia@gmail.com>
2024-03-17 17:47:51 -0600

    Primer commit: carga inicial del codigo

---
+++ src/main/java/practica/propia/prueba/Prueba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package practica.propia.prueba;
+
+/**
+ *
+ * @author lenov
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
